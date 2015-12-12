2dee57390d8bf3caf96b5cf4f8eca2838ecf3d8c
Denis Tran <denis.tran@gmail.com>
2015-12-12 10:28:49 -0800

    first commit

---
+++ TestTrees/TestTrees/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestTrees
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //#region binary tree test
+            //TreeNode one = new TreeNode(1, null);
+            //TreeNode two = new TreeNode(2, one);
+            //TreeNode three = new TreeNode(3, one);
+            //TreeNode four = new TreeNode(4, two);
+            //TreeNode five = new TreeNode(5, two);
+            //TreeNode six = new TreeNode(6, three);
+            //TreeNode seven = new TreeNode(7, three);
+            //one.Left = two;
+            //one.Right = three;
+            //two.Left = four;
+            //two.Right = five;
+            //three.Left = six;
+            //three.Right = seven;
+            
+            //BinaryTree tree7 = new BinaryTree(seven, null, null);
+            //BinaryTree tree6 = new BinaryTree(six, null, null);
+            //BinaryTree tree5 = new BinaryTree(five, null, null);
+            //BinaryTree tree4 = new BinaryTree(four, null, null);
+            //BinaryTree tree3 = new BinaryTree(three, tree6, tree7);
+            //BinaryTree tree2 = new BinaryTree(two, tree4, tree5);
+            //BinaryTree tree1 = new BinaryTree(one,tree2, tree3);
+
+            ////BinaryTree.PreOrder(tree1);
+            ////Console.WriteLine();
+            ////BinaryTree.InOrder(tree1);
+            ////Console.WriteLine();
+            ////BinaryTree.PostOrder(tree1);
+            ////Console.WriteLine();
+
+            //BinaryTree.BreadthFirst(tree1);
+
+            //#endregion
+
+            // populate a binary search tree
+
+
+        }
+    }
+}
---
+++ TestTrees/TestTrees/TreeNode.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTrees
+{
+    public class TreeNode
+    {
+        private int value;
+        public int Value
+        {
+          get { return this.value; }
+          set { this.value = value; }
+        }
+
+        private TreeNode parent;
+        internal TreeNode Parent
+        {
+            get { return parent; }
+            set { parent = value; }
+        }
+
+        private TreeNode left;
+        internal TreeNode Left
+        {
+            get { return left; }
+            set { left = value; }
+        }
+
+        private TreeNode right;
+        internal TreeNode Right
+        {
+            get { return right; }
+            set { right = value; }
+        }
+
+        public TreeNode(int value, TreeNode par)
+        {
+            this.value = value;
+            this.parent = par;
+        }
+
+        public TreeNode(TreeNode node)
+        {
+            this.value = node.value;
+            this.parent = node.parent;
+        }
+
+        public void Visit()
+        {
+            Console.Write(value.ToString());
+        }
+
+        public string ToString()
+        {
+            return this.value.ToString();
+        }
+
+    }
+
+    public class BinaryTree
+    {
+        private TreeNode root;
+        internal TreeNode Root
+        {
+            get { return root; }
+            set { root = value; }
+        }
+
+        private BinaryTree leftTree;
+        public BinaryTree LeftTree
+        {
+            get { return leftTree; }
+            set { leftTree = value; }
+        }
+
+        private BinaryTree rightTree;
+        public BinaryTree RightTree
+        {
+            get { return rightTree; }
+            set { rightTree = value; }
+        }
+
+        public BinaryTree()
+        {
+
+        }
+
+        public BinaryTree(BinaryTree tree)
+        {
+            this.root = tree.root;
+            this.leftTree = tree.leftTree;
+            this.rightTree = tree.rightTree;
+        }
+
+        public BinaryTree(TreeNode root, BinaryTree left, BinaryTree right)
+        {
+            this.root = root;
+            this.leftTree = left;
+            this.rightTree = right;
+
+        }
+
+        public string ToString()
+        {
+            return this.root.Value.ToString();
+        }
+
+
+        // traversal methods
+        // depth first
+        public static void PreOrder(BinaryTree tree)
+        {
+            if (tree != null)
+            {
+                tree.Root.Visit();
+                PreOrder(tree.leftTree);
+                PreOrder(tree.rightTree);
+            }
+        }
+
+        public static void InOrder(BinaryTree tree)
+        {
+            if (tree != null)
+            {
+                InOrder(tree.leftTree);
+                tree.Root.Visit();
+                InOrder(tree.rightTree);
+            }
+        }
+
+        public static void PostOrder(BinaryTree tree)
+        {
+            if (tree != null)
+            {
+                PostOrder(tree.leftTree);
+                PostOrder(tree.rightTree);
+                tree.Root.Visit();
+            }
+        }
+
+        public static void BreadthFirst(BinaryTree tree)
+        {
+            Queue<TreeNode> q = new Queue<TreeNode>();
+            q.Enqueue(tree.root);
+            while (q.Count!=0)
+            {
+                TreeNode current = q.Dequeue();
+                if (current != null)
+                {
+                    current.Visit();
+                    q.Enqueue(current.Left);
+                    q.Enqueue(current.Right);
+                }
+            }
+        }
+
+    }
+
+    public class BinarySearchTree
+    {
+        // same as Binary Tree but add functions for search, insert, delete
+        private TreeNode FindBST(int value)
+        {// assume the tree is BST
+
+        }
+
+        // check that it is BST
+
+        // make this into a BST
+
+        // insert into BST
+
+        // delete from a BST
+    }
+}
